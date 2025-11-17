369a9996ed0d07139fe5a1e40243e3d1b47edc5b
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-11-17 14:31:59 +0100

    form corrections

--- app/components/form.tsx
+++ app/components/form.tsx
@@ -16,50 +16,61 @@ export function FormSubmit() {
   const [switch1, setSwitch1] = useState(false);
   return (
     <div className="m-10 max-w-md items-center justify-center">
-      <div className="mb-2 block">
-        <Label htmlFor="Nombre">Nombre y apellido</Label>
+      <div className="my-4">
+        <div className="mb-2 block">
+          <Label htmlFor="Nombre">Nombre y apellido</Label>
+        </div>
+        <TextInput
+          id="Nombre"
+          placeholder="Nombre y apellido"
+          addon={<FaUser />}
+          required
+        />
       </div>
-      <TextInput
-        id="Nombre"
-        placeholder="Nombre y apellido"
-        addon={<FaUser />}
-        required
-      />
-      <div className="mb-2 block">
-        <Label htmlFor="Empresa">Empresa</Label>
+      <div className="my-4">
+        <div className="mb-2 block">
+          <Label htmlFor="Empresa">Empresa</Label>
+        </div>
+        <TextInput
+          id="Empresa"
+          placeholder="Empresa"
+          addon={<FaStore />}
+          required
+        />
       </div>
-      <TextInput
-        id="Empresa"
-        placeholder="Empresa"
-        addon={<FaStore />}
-        required
-      />
-      <div className="mb-2 block">
-        <Label htmlFor="Cargo">Cargo</Label>
+      <div className="my-4">
+        <div className="mb-2 block">
+          <Label htmlFor="Cargo">Cargo</Label>
+        </div>
+        <TextInput
+          id="Cargo"
+          placeholder="Cargo"
+          addon={<FaAddressCard />}
+          required
+        />
       </div>
-      <TextInput
-        id="Cargo"
-        placeholder="Cargo"
-        addon={<FaAddressCard />}
-        required
-      />
-      <div className="mb-2 block">
-        <Label htmlFor="Email">Email</Label>
+      <div className="my-4">
+        <div className="mb-2 block">
+          <Label htmlFor="Email">Email</Label>
+        </div>
+        <TextInput
+          id="Email"
+          placeholder="Email"
+          addon={<FaEnvelope />}
+          required
+        />
       </div>
-      <TextInput
-        id="Email"
-        placeholder="Email"
-        addon={<FaEnvelope />}
-        required
-      />
-      <div className="mb-2 block">
-        <Label htmlFor="Numero">Numero de telefono</Label>
+      <div className="my-4">
+        <div className="mb-2 block">
+          <Label htmlFor="Numero">Numero de telefono</Label>
+        </div>
+        <TextInput
+          id="Numero"
+          placeholder="Numero de telefono"
+          addon={<FaPhoneAlt />}
+        />
       </div>
-      <TextInput
-        id="Numero"
-        placeholder="Numero de telefono"
-        addon={<FaPhoneAlt />}
-      />
+
       <div className="my-3 flex h-full w-full flex-row justify-center gap-10 rounded-2xl border border-gray-200 bg-white p-3 align-middle">
         <Chalkboard className="h-15 rounded-2xl bg-gray-100 p-2" />
         <div className="flex flex-col gap-2 rounded-3xl align-middle">
@@ -73,7 +84,9 @@ export function FormSubmit() {
           </p>
         </div>
       </div>
-      <Button>Solicitar demo</Button>
+      <div className="flex justify-center">
+        <Button className="bg-amber-400 tracking-wide">Solicitar demo</Button>
+      </div>
     </div>
   );
 }
