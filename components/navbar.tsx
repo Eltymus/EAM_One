8b15711654c28e12408a3474894865bf9398b3f5
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2025-11-19 13:52:54 +0100

    fix mini css

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,26 +7,30 @@ import {
   NavbarToggle,
 } from "flowbite-react";
 
+import { motion } from "motion/react";
+
 export function Nav() {
   return (
-    <Navbar fluid>
-      <NavbarBrand href="">
-        <img src="EAMONElogo.png" className="m-3 h-10" alt="EAM ONE Logo" />
-      </NavbarBrand>
-      <NavbarCollapse>
-        <NavbarLink href="#advantages">Ventajas</NavbarLink>
-        <NavbarLink href="#faq">FAQs</NavbarLink>
-        <NavbarLink href="#contact">Contacto</NavbarLink>
-      </NavbarCollapse>
-      <div className="flex">
-        <Button
-          href="#contact"
-          className="bg-amber-500 delay-150 duration-150 hover:bg-purple-500"
-        >
-          Solicitar demo
-        </Button>
-        <NavbarToggle />
-      </div>
-    </Navbar>
+    <motion.div>
+      <Navbar fluid>
+        <NavbarBrand href="">
+          <img src="EAMONElogo.png" className="m-3 h-10" alt="EAM ONE Logo" />
+        </NavbarBrand>{" "}
+        <div className="flex md:order-2">
+          <Button
+            href="#contact"
+            className="bg-amber-500 delay-150 duration-150 hover:bg-purple-500"
+          >
+            Solicitar demo
+          </Button>
+          <NavbarToggle />
+        </div>
+        <NavbarCollapse>
+          <NavbarLink href="#advantages">Ventajas</NavbarLink>
+          <NavbarLink href="#faq">FAQs</NavbarLink>
+          <NavbarLink href="#contact">Contacto</NavbarLink>
+        </NavbarCollapse>
+      </Navbar>
+    </motion.div>
   );
 }
